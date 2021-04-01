43377dcc1c4731974539493095f2441a8f3350eb
knecht <ldj910505@naver.com>
2021-04-02 01:39:32 +0900

    자바스크립트 문제 1개 풂

--- 1001.js
+++ 1001.js
@@ -1,15 +1,15 @@
 const readline = require('readline');
 const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+	input: process.stdin,
+	output: process.stdout
 });
 
 let input = [];
 
 reader.on('line', function (line) {
-    input = line.split(' ').map((el) => parseInt(el));
-    reader.close();
+	input = line.split(' ').map((el) => parseInt(el));
+	reader.close();
 }).on('close', function () {
-    console.log(input[0] - input[1]);
-    process.exit();
+	console.log(input[0] - input[1]);
+	process.exit();
 });
